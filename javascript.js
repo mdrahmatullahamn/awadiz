6104fbec8b0bb4cbfdf72b549589b02bf55bb06e
mdrahmatullahamn <rmd831219@gmail.com>
2023-01-30 14:06:41 +0530

    pagelayout

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
-9065
-var amaan =9065
-console.log(amaan)
+// 9065
+// var shad =9065;
+// console.log(shad);
+// console.log("data type:",typeof(shad))
+// var amaan = false;
+// console.log(amaan)
+// console.log("data type:",typeof(amaan))
+// var data ="ghjjjjj";
+// console.log("data")
+// var f= 10
+// var s= 8
+// console.log(10==8)
+// var f= 10
+// var s= 10
+// console.log(f == s)
+console.log(2!=2)
+console.log (2!=3)
